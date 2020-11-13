855877cb21f7805a257dde21be9a38482c891b23
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-11-13 18:12:08 +0000

    initial source commit

---
+++ ArmedScopeGuard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ScopeGuard
+{
+    public class ArmedScopeGuard : ScopeGuard
+    {
+        private bool _armed = true;
+
+        public ArmedScopeGuard(Action callback)
+            : base(callback)
+        {
+        }
+
+        public void Disarm()
+        {
+            _armed = false;
+        }
+
+        // ^IDisposable
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && _armed)
+                Callback();
+        }
+    }
+}
---
+++ ScopeGuard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace ScopeGuard
+{
+    public class ScopeGuard : IDisposable
+    {
+        protected readonly Action Callback;
+
+        public ScopeGuard(Action callback)
+        {
+            Callback = callback ?? throw new ArgumentException(nameof(callback));
+        }
+
+        // ^IDisposable
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        // ^IDisposable
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                Callback();
+        }
+    }
+}
